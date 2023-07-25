ca7143177af088416ed4dcf4b7c094a2f7a589c7
fokhrun <rupa0609@gmail.com>
2023-07-25 04:12:03 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Questions and Answers
-const questions = [
+// Sample quiz data
+const quizData = [
     {
         question: "Which country won the 2010 FIFA World Cup?",
         options: ["Germany", "Brazil", "France", "Spain"],
@@ -24,13 +24,21 @@ const questions = [
         correctAnswer: "Argentina",
         image: "images/argentina.jpg"
     },
-];
+  ];
+  
+  const quizContainer = document.getElementById("quiz-container");
+  const questionElement = document.getElementById("question");
+  const optionsElement = document.getElementById("options");
+  const submitButton = document.getElementById("submit-btn");
+  const resultContainer = document.getElementById("result-container");
+  const resultsList = document.getElementById("results");
+  const restartButton = document.getElementById("restart-btn");
+  
+  let currentQuestion = 0;
+  let score = 0;
+  let username = "";
 
-let currentQuestionIndex = 0;
-let score = 0;
-let username = "";
-
-function startQuiz() {
+  function startQuiz() {
     username = document.getElementById("username").value;
     if (!username) {
         alert("Please enter a username before starting the quiz.");
@@ -42,55 +50,64 @@ function startQuiz() {
     showQuestion();
 }
 
-function showQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        const questionElem = document.getElementById("question");
-        const optionsElem = document.getElementById("options");
+  
+  function showQuestion() {
+    const currentQuizData = quizData[currentQuestion];
+    questionElement.textContent = currentQuizData.question;
+    optionsElement.innerHTML = "";
+    currentQuizData.options.forEach((option) => {
+      const label = document.createElement("label");
+      const radioInput = document.createElement("input");
+      radioInput.type = "radio";
+      radioInput.name = "answer";
+      radioInput.value = option;
+      label.appendChild(radioInput);
+      label.appendChild(document.createTextNode(option));
+      optionsElement.appendChild(label);
+    });
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="answer"]:checked');
+    if (!selectedOption) return;
+  
+    const userAnswer = selectedOption.value;
+    const currentQuizData = quizData[currentQuestion];
+    const correctAnswer = currentQuizData.correctAnswer;
 
-        questionElem.innerHTML = questions[currentQuestionIndex].question;
-
-        optionsElem.innerHTML = "";
-        for (let i = 0; i < questions[currentQuestionIndex].options.length; i++) {
-            optionsElem.innerHTML += `<button onclick="checkAnswer('${questions[currentQuestionIndex].options[i]}')">${questions[currentQuestionIndex].options[i]}</button>`;
-        }
-
-        document.getElementById("next-btn").style.display = "none";
-        document.getElementById("feedback").innerHTML = "";
-    } else {
-        showScore();
-    }
-}
-
-function checkAnswer(selectedOption) {
-    const correctAnswer = questions[currentQuestionIndex].correctAnswer;
-    const feedbackElem = document.getElementById("feedback");
-
-    if (selectedOption === correctAnswer) {
-        feedbackElem.innerHTML = "<img src='images/correct.png' alt='Correct'>";
+    if (userAnswer === correctAnswer) {
+        //feedbackElem.innerHTML = "<img src='images/correct.png' alt='Correct'>";  
         score++;
-    } else {
-        feedbackElem.innerHTML = `<img src='images/incorrect.png' alt='Incorrect'><p>Correct answer: ${correctAnswer}</p>`;
     }
-
-    document.getElementById("next-btn").style.display = "block";
-}
-
-function nextQuestion() {
-    currentQuestionIndex++;
-    showQuestion();
-}
-
-function showScore() {
-    document.getElementById("quiz-section").style.display = "none";
-    document.getElementById("score-section").style.display = "block";
-    document.getElementById("score").innerText = score;
-}
-
-function resetQuiz() {
-    currentQuestionIndex = 0;
+    /*else {
+        //feedbackElem.innerHTML = `<img src='images/incorrect.png' alt='Incorrect'><p>Correct answer: ${correctAnswer}</p>`;
+    }*/
+  
+    currentQuestion++;
+  
+    if (currentQuestion < quizData.length) {
+      showQuestion();
+    } else {
+      showResults();
+    }
+  }
+  
+  function showResults() {
+    quizContainer.style.display = "none";
+    resultContainer.style.display = "block";
+    resultContainer.innerHTML = `<h2>Results:</h2><ul id="results"></ul> ${username} scored ${score} out of ${quizData.length}`;
+  }
+  
+  function restartQuiz() {
+    currentQuestion = 0;
     score = 0;
-    username = "";
-    document.getElementById("user-section").style.display = "block";
-    document.getElementById("score-section").style.display = "none";
-    startQuiz();
-}
+    quizContainer.style.display = "block";
+    resultContainer.style.display = "none";
+    showQuestion();
+  }
+  
+  submitButton.addEventListener("click", checkAnswer);
+  restartButton.addEventListener("click", restartQuiz);
+  
+  showQuestion();
+  
